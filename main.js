2c532e3cbd6ea749925adbda471713f709c3b774
aahelexe <78219629+aahelexe@users.noreply.github.com>
2022-02-16 21:01:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+harry_potter="";
+peter_pan=""; 
+current_song="";
+song1="";
+song2="";
+lWx=0;
+lWy=0;
+rWx=0;
+rWy=0;
+rWscore=0;
+lWscore=0;
+function preload()
+{
+    harry_potter=loadSound("harrypotter.mp3");
+    peter_pan=loadSound("peterpan.mp3");
+}
+function setup()
+{
+    canvas=createCanvas(600,400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log("PoseNET ready.");
+}
+function gotPoses(results,error)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    if(results.length>0)
+    {
+        lWy=results[0].pose.leftWrist.y;
+        lWx=results[0].pose.leftWrist.x;
+        rWx=results[0].pose.rightWrist.x;
+        rWy=results[0].pose.rightWrist.y;
+        lWscore=results[0].pose.keypoints[9].score;
+        console.log("LeftWy= "+lWy+" LeftWx= "+lWx+" RightWy= "+rWy+" RightWx= "+rWx);
+        console.log("LeftWScore= "+lWscore);
+    }
+}
+function draw()
+{
+    image(video,0,0,600,400);
+    song1=harry_potter.isPlaying();
+    song2=peter_pan.isPlaying();
+    if(lWscore>0.2)
+    {
+        fill("red");
+        circle(lWx,lWy,20,20);
+        peter_pan.stop();
+        if(song1==false)
+        {
+            harry_potter.play();
+            document.getElementById("song").innerHTML="Song: Harry Potter";
+        }
+    }
+}
